4e36d1c38fdfe144e6640666a1a0f1bf5d2fef47
rchellee <ruerasritchelle07@gmail.com>
2024-01-22 17:46:54 +0800

    Update App.js
    revert error

--- src/App.js
+++ src/App.js
@@ -5,33 +5,50 @@ import Home from './Home';
 import Explore from './Explore';
 import Login from './Login';
 import SignUp from './SignUp';
+import Logout from './Logout';
 import CreateEvent from './CreateEvent';
 import EventDetails from './EventDetails';
 import EventProponents from './EventProponents';
+import { useAuth } from './AuthContext';
 
 
 function App() {
+  const { isAuthenticated, logout } = useAuth(); // Add the 'logout' function from the AuthContext
+
+  const handleLogout = () => {
+    // Call the 'logout' function to log the user out
+    logout();
+  };
+
   return (
-    <Router class = "header">
-      <div className="navbar">
-        <span>Event</span>Management
-        <nav>
+    <Router>
+      <div className="App">
+      <nav>
           <ul>
             <li>
               <Link to="/">Home</Link>
             </li>
             <li>
-              <Link to="/explore">About</Link>
+              <Link to="/explore">Explore</Link>
             </li>
-            <li>
-              <Link to="/login">Login</Link>
-            </li>
-            <li>
-              <Link to="/signup">Sign Up</Link>
-            </li>
-            <li>
-              <Link to="/create-event">Create Event</Link>
-            </li>
+            {isAuthenticated ? (
+              <>
+                {/* Display these links only if the user is authenticated */}
+                <li>
+                  <Link to="/logout">Logout</Link>
+                </li>
+              </>
+            ) : (
+              <>
+                {/* Display these links only if the user is not authenticated */}
+                <li>
+                  <Link to="/login">Login</Link>
+                </li>
+                <li>
+                  <Link to="/signup">Sign Up</Link>
+                </li>
+              </>
+            )}
           </ul>
         </nav>
 
@@ -44,6 +61,7 @@ function App() {
           <Route path="/event-details/:eventName" element={<EventDetails />} />
           {/* Example route for event proponents */}
           <Route path="/event-proponents" element={<EventProponents proponents={['Organizer 1', 'Organizer 2']} />} />
+          <Route path="/logout" element={<Logout />} />
           <Route path="/" element={<Home />} />
         </Routes>
       </div>
