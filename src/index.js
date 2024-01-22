939440dca1d9b847f2888554cf56f26fff65d61c
rchellee <ruerasritchelle07@gmail.com>
2024-01-22 14:17:54 +0800

    Revert "js cc js"
    This reverts commit 31f356ae583ee02a31660d9f472cf48618bcfd4d.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
-body {
-  margin: 0;
-  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
-    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
-    sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-}
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { AuthProvider } from './AuthContext';
+import App from './App';
 
-code {
-  font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
-    monospace;
-}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <AuthProvider>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </AuthProvider>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
